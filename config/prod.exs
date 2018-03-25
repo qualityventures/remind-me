cc1d76720335d12c7549711fc5f632425ba331b7
Damon Janis <damonvjanis@gmail.com>
2018-03-25 12:47:46 -0600

    Adjust prod config for nginx reverse proxy

--- config/prod.exs
+++ config/prod.exs
@@ -1,10 +1,11 @@
 use Mix.Config
 
 config :remind_me, RemindMeWeb.Endpoint,
-  http: [port: 80],
-  url: [host: "www.remindme.live"],
+  http: [port: 4000],
+  url: [host: "remindme.live", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
+  code_reloader: false,
   root: ".",
   version: Application.spec(:remind_me, :vsn)
 
