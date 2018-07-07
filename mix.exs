1d3c30e6e6694a861ccfc068c77c192c505c4264
Damon Janis <damonvjanis@gmail.com>
2018-07-06 21:32:47 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RemindMe.Mixfile do
   def project do
     [
       app: :remind_me,
-      version: "1.0.7",
+      version: "1.0.8",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
