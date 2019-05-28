243c4e7b7ed4b2daeb05b2d1eb1b50ff822dd7e0
Damon Janis <damonvjanis@gmail.com>
2019-05-28 08:10:27 -0600

    Fix Sentry config

--- config/config.exs
+++ config/config.exs
@@ -12,8 +12,7 @@ config :remind_me, RemindMeWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "QqMCQHv+l5yWvwGkis2kJ7GZsSXGuJ+hdj5hkICHEJYSYyRibPNLQfI9F38ygPlp",
   render_errors: [view: RemindMeWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: RemindMe.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: RemindMe.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Phauxth authentication configuration
 config :phauxth,
@@ -22,13 +21,20 @@ config :phauxth,
 
 # Configures the Sendgrid mailer
 config :remind_me, RemindMe.Mailer,
-adapter: Bamboo.SendGridAdapter,
-api_key: System.get_env("SENDGRID_API_KEY")
+  adapter: Bamboo.SendGridAdapter,
+  api_key: System.get_env("SENDGRID_API_KEY")
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+
+# Configures Sentry error logging
+config :sentry,
+  dsn: "https://08e4991d3cc84c80a1d4c6ed8dc92088@sentry.io/433417",
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!,
+  included_environments: [:prod]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- config/prod.exs
+++ config/prod.exs
@@ -12,16 +12,6 @@ config :remind_me, RemindMeWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :sentry,
-  dsn: "https://08e4991d3cc84c80a1d4c6ed8dc92088@sentry.io/433417",
-  environment_name: :prod,
-  enable_source_code_context: true,
-  root_source_code_path: File.cwd!,
-  tags: %{
-    env: "production"
-  },
-  included_environments: [:prod]
-
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
 import_config "prod.secret.exs"
